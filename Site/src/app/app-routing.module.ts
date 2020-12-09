b70921b0f39e279c88c6817ab1cdc7b156ee4874
ricardo.cruz29 <ricardo.cruz29@ua.pt>
2020-12-09 19:58:52 +0000

    Web initialization on angular

---
+++ Site/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { StatisticsComponent } from './statistics/statistics.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent} from './login/login.component';
+
+const routes: Routes = [
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
