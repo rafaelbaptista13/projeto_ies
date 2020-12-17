4c9b982f27bfde7272ed20f8e8010e6e64b89fa9
Rafael Baptista <rafaelbaptista@ua.pt>
2020-12-17 01:52:50 +0000

    Number of cases by current state

--- Covinfo19/src/main/java/ua/ies/g23/Covinfo19/relatorios/repository/CasoRepository.java
+++ Covinfo19/src/main/java/ua/ies/g23/Covinfo19/relatorios/repository/CasoRepository.java
@@ -1,6 +1,9 @@
 package ua.ies.g23.Covinfo19.relatorios.repository;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ua.ies.g23.Covinfo19.relatorios.model.Caso;
@@ -8,5 +11,23 @@ import ua.ies.g23.Covinfo19.relatorios.model.Caso;
 
 @Repository
 public interface CasoRepository extends JpaRepository<Caso, Long>{
+    @Query(value = "Select count(*) from casos where estado_atual = 'Confinamento Domiciliário'", nativeQuery = true)
+	Collection<Integer> findAllConfinamento();
+
+    @Query(value = "Select count(*) from casos where estado_atual = 'Internamento'", nativeQuery = true)
+	Collection<Integer> findAllInternado();
+
+    @Query(value = "Select count(*) from casos where estado_atual = 'Morto'", nativeQuery = true)
+	Collection<Integer> findAllMorto();
+
+    @Query(value = "Select count(*) from casos where estado_atual = 'Cuidados Intensivos'", nativeQuery = true)
+	Collection<Integer> findAllCuidados();
+
+    @Query(value = "Select count(*) from casos where estado_atual = 'Cuidados Intensivos' or estado_atual = 'Internamento' or estado_atual = 'Confinamento Domiciliário'", nativeQuery = true)
+	Collection<Integer> findAllAtivos();
+
+    @Query(value = "Select count(*) from casos where estado_atual = 'Recuperado'", nativeQuery = true)
+	Collection<Integer> findAllRecuperados();
+   
 
 }
