5488a39137a8ace0a42845d1fdbe1bb417f410f8
Pedro Amaral <pedromiglou@hotmail.com>
2020-12-18 01:11:08 +0000

    connection between api and daily statistics webpage

---
+++ Site/src/app/paciente.ts
@@ -0,0 +1,14 @@
+/*
+----old code, needs review-------
+export class Paciente {
+  numeroUtente: number;
+  genero: string;
+  idade: number;
+  nome: string;
+  concelho: string;
+  regiao: string;
+  nacionalidade: string;
+  altura: number;
+  peso: number;
+}
+ */
--- Site/src/app/statistics/statistics.component.ts
+++ Site/src/app/statistics/statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CasosService} from '../casos.service';
 
 declare var jQuery: any;
 declare const CanvasJS: any;
@@ -10,10 +11,15 @@ declare const CanvasJS: any;
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  casosAtivos: number;
+  casosRecuperados: number;
+  casosCuidadosIntensivos: number;
+  casosMortos: number;
 
-  constructor() { }
+  constructor(private casosService: CasosService) { }
 
   ngOnInit(): void {
+
     (function($) {
       $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
@@ -22,8 +28,13 @@ export class StatisticsComponent implements OnInit {
         });
       })
     })(jQuery);
-    
-    charts()
+
+    charts();
+    this.casosService.getNumeroCasos('ativos').subscribe(casosAtivos => this.casosAtivos = casosAtivos);
+    this.casosService.getNumeroCasos('recuperados').subscribe(casosRecuperados => this.casosRecuperados = casosRecuperados);
+    this.casosService.getNumeroCasos('cuidadosintensivos')
+      .subscribe(casosCuidadosIntensivos => this.casosCuidadosIntensivos = casosCuidadosIntensivos);
+    this.casosService.getNumeroCasos('morto').subscribe(casosMortos => this.casosMortos = casosMortos);
   }
 }
 
@@ -166,73 +177,6 @@ function charts() {
     }]
   });
   chart.render();
-
-
-
-  // grafico casos ativos
-  var chart = new CanvasJS.Chart("activecasesgraph", {
-    animationEnabled: true,
-    title:{
-      text: ""
-    },
-    axisX:{
-      valueFormatString: "DD MMM",
-      crosshair: {
-        enabled: true,
-        snapToDataPoint: true
-      }
-    },
-    axisY: {
-      title: "Número de casos",
-      valueFormatString: "##",
-      crosshair: {
-        enabled: true,
-        snapToDataPoint: true,
-        labelFormatter: function(e) {
-          return CanvasJS.formatNumber(e.value, "##") + " infeções";
-        }
-      }
-    },
-    data: [{
-      type: "area",
-      xValueFormatString: "DD MMM",
-      yValueFormatString: "##",
-      dataPoints: [
-        { x: new Date(2020, 10, 1), y: 430 },
-        { x: new Date(2020, 10, 2), y: 493 },
-        { x: new Date(2020, 10, 3), y: 329 },
-        { x: new Date(2020, 10, 4), y: 830 },
-        { x: new Date(2020, 10, 5), y: 634 },
-        { x: new Date(2020, 10, 6), y: 534 },
-        { x: new Date(2020, 10, 7), y: 644 },
-        { x: new Date(2020, 10, 8), y: 569 },
-        { x: new Date(2020, 10, 9), y: 695 },
-        { x: new Date(2020, 10, 10), y: 938 },
-        { x: new Date(2020, 10, 11), y: 1340 },
-        { x: new Date(2020, 10, 12), y: 1032 },
-        { x: new Date(2020, 10, 13), y: 983 },
-        { x: new Date(2020, 10, 14), y: 1332 },
-        { x: new Date(2020, 10, 15), y: 1284 },
-        { x: new Date(2020, 10, 16), y: 1623 },
-        { x: new Date(2020, 10, 17), y: 2303 },
-        { x: new Date(2020, 10, 18), y: 1032 },
-        { x: new Date(2020, 10, 19), y: 2032 },
-        { x: new Date(2020, 10, 20), y: 3032 },
-        { x: new Date(2020, 10, 21), y: 3432 },
-        { x: new Date(2020, 10, 22), y: 4973 },
-        { x: new Date(2020, 10, 23), y: 6948 },
-        { x: new Date(2020, 10, 24), y: 4938 },
-        { x: new Date(2020, 10, 25), y: 5829 },
-        { x: new Date(2020, 10, 26), y: 6483 },
-        { x: new Date(2020, 10, 27), y: 7282 },
-        { x: new Date(2020, 10, 28), y: 5647 },
-        { x: new Date(2020, 10, 29), y: 4479 }
-      ]
-    }]
-  });
-  chart.render();
-
-
 }
 
 function explodePie (e) {
