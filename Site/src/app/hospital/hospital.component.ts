5eb58102c04aea8facb025f31ccc117dceaa0681
Ricardo <ricardo.cruz29@ua.pt>
2020-12-19 01:22:30 +0000

    Prototype of hospital page

---
+++ Site/src/app/hospital/hospital.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital',
+  templateUrl: './hospital.component.html',
+  styleUrls: ['./hospital.component.css']
+})
+export class HospitalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //Função para colapsar navbar
+    (function($) {
+      $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+        });
+      })
+    })(jQuery);
+
+    //Função para colapsar aba de filtros
+    (function($) {
+      $(document).ready(function () {
+        $('#filterCollapse').on('click', function () {
+          $('#filters').toggle(500);
+        });
+      })
+    })(jQuery);
+
+  }
+
+}
