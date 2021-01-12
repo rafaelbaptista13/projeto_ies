8a0b1afedaaf359381ca77b15c0236df5fdc7ca2
Pedro Amaral <pedromiglou@hotmail.com>
2021-01-12 11:49:56 +0000

    various dockerfiles and docker compose up

--- Site/src/app/casos.service.ts
+++ Site/src/app/casos.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 //Define mensagem em JSON e o URL é de segurança
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json', 'access-control-allow-origin': 'http://localhost:8080/'})
+  headers: new HttpHeaders({'Content-Type': 'application/json', 'access-control-allow-origin': 'http://192.168.160.215:8080/'})
 };
 
 @Injectable({
@@ -13,7 +13,7 @@ const httpOptions = {
 })
 export class CasosService {
   //URL inicial da api
-  private baseURL = 'http://localhost:8080/api/v1/';
+  private baseURL = 'http://192.168.160.215:8080/api/v1/';
 
   constructor(private http: HttpClient) { }
 
