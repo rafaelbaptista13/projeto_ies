7f1655a8f74b12db502a0d4f0937a7f9da1cf278
Pedro Amaral <pedromiglou@hotmail.com>
2021-01-12 22:32:03 +0000

    Paginators Implemented

--- Covinfo19/src/main/java/ua/ies/g23/Covinfo19/pacientes_med_hosp/repository/MedicoRepository.java
+++ Covinfo19/src/main/java/ua/ies/g23/Covinfo19/pacientes_med_hosp/repository/MedicoRepository.java
@@ -2,6 +2,7 @@ package ua.ies.g23.Covinfo19.pacientes_med_hosp.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +15,10 @@ import ua.ies.g23.Covinfo19.pacientes_med_hosp.model.Medico;
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
     //Seleção de todos os médicos através de filtros pelos seus campos
     @Query(value = "Select * from medicos where nome like :strnome and hospital_id like :strhospital_id and idade between :stridademin and :stridademax", nativeQuery = true)
-	List<Medico> findAllByFilters(@Param("strnome") String strnome,@Param("strhospital_id") String strhospital_id,@Param("stridademin") String stridademin,@Param("stridademax") String stridademax);
+    List<Medico> findAllByFilters(@Param("strnome") String strnome,@Param("strhospital_id") String strhospital_id,@Param("stridademin") String stridademin,@Param("stridademax") String stridademax);
+    
+    @Query(value = "Select * from medicos where nome like :strnome and hospital_id like :strhospital_id and idade between :stridademin and :stridademax /* #pageReq */", nativeQuery = true)
+	List<Medico> findAllByFilters(@Param("strnome") String strnome,@Param("strhospital_id") String strhospital_id,@Param("stridademin") String stridademin,@Param("stridademax") String stridademax, @Param("pageReq") PageRequest pageRequest);
 
     @Query(value = "Select * from medicos where numero_medico like :numeromedico", nativeQuery = true)
     Medico findByNumeroMedico(@Param("numeromedico") String numeromedico);
